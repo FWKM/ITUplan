ee91c0e685c8aae86092c7a241088a0caaa2c326
Wordenskjold <frederik@wordenskjold.dk>
2011-02-16 12:23:52 +0100

    added some login code

---
+++ src/main/scala/snippet/Login.scala
@@ -0,0 +1,21 @@
+package code.snippet
+
+import _root_.net.liftweb._
+import http._
+import _root_.net.liftweb.util._
+import _root_.net.liftweb.common._
+import _root_.scala.xml._
+import _root_.net.liftweb.util.BindHelpers._
+import _root_.net.liftweb.http.SHtml._
+import _root_.net.liftweb.http.js.JsCmds._
+import _root_.net.liftweb.http.js.JE._
+import _root_.net.liftweb.http._
+
+object Init {
+	
+	def render(html : NodeSeq) : NodeSeq = {
+		def b = false
+		if(!b) <lift:embed what="login" />
+		else <lift:embed what="bookings_screen" />
+	}
+}
